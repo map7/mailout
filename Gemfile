dc956b599d6abf06b0f5a7d294afc6856737b457
Michael Pope <map7@corsairsolutions.com.au>
2013-08-08 23:21:44 +1000

    Update the_merger with html ability and letters -> show now gives a preview

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ gem 'jquery-rails'
 # gem 'capistrano'
 
 # To use debugger
-gem 'debugger'
+#gem 'debugger'
 
 gem 'inherited_resources'
 gem 'simple-navigation'
--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,8 +1,17 @@
 class LettersController < InheritedResources::Base
+  require 'redcarpet'
+
   def create
     create!{letters_path}
   end
 
+  def show
+    @letter = Letter.find(params[:id])
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
+    @body = markdown.render(@letter.body)
+    
+  end
+  
   def update
     update!{letters_path}
   end
